26f6e95be5c2e1e1ef16d95e12276fe83aab95bb
venkatraman1 <venkat94432@gmail.com>
2021-12-17 09:33:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+import Search from './components/Search'
+import Header from './components/Header';
+
+function App() {
+  const notesData = [
+    {
+      id: nanoid(),
+      text: 'this is first note',
+      date: '11/12/2021'
+    },
+    {
+      id: nanoid(),
+      text: 'this is second note',
+      date: '12/12/2021'
+    },
+    {
+      id: nanoid(),
+      text: 'this is third note',
+      date: '13/12/2021'
+    }
+];
+  const [notes, setNotes] = useState(notesData);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const addNote = (text) =>{
+    const date = new Date();
+    const newNote={
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  }
+
+  const handleDeleteNote = (id) =>{
+    const remainingNotes = notes.filter((note) => note.id != id);
+    setNotes(remainingNotes);
+  }
+
+  const [searchText, SetSearchText] = useState('');
+
+ useEffect(()=>{
+   const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'));
+
+   if(savedNotes){
+     setNotes(savedNotes);
+   }
+ }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('react-notes-app-data', JSON.stringify(notes))
+  },[notes])
+
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+      <Header handleDarkMode={setDarkMode} />
+      <Search handleSearchText={SetSearchText} />
+      <NotesList
+        notes={notes.filter((note)=>note.text.includes(searchText))}
+        handleAddNote={addNote}
+        handleDeleteNote = {handleDeleteNote}
+      />
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+const Header = ({handleDarkMode}) =>{
+    return(
+        <div className="header">
+            <h1>Notes</h1>
+            <button className="save" onClick={()=>handleDarkMode((prevDarkMode)=>!prevDarkMode)}>Toggle Mode</button>
+        </div>
+    );
+}
+
+export default Header;
